d5fe77924b0538f6535ad8bcd776f335557ae2be
“[Nicol <[nicol.saha@gmail.com]>
2020-12-23 19:10:57 +0100

    Set up redux store and provider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useEffect, Fragment, Profiler } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import SearchBar from './components/layout/SearchBar';
@@ -10,22 +10,27 @@ import EditLogModal from './components/logs/EditLogModal';
 import TechListModal from './components/techs/TechListModal';
 import './App.css';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 const App = () => {
   useEffect(() => {
     M.AutoInit();
   });
   return (
-    <Fragment>
-      <SearchBar />
-      <div className='container'>
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 };
 
